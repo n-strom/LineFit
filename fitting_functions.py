85cd3141c155fc77be5c7107ef2c966298143900
n-strom <nstrom@eagles.bridgewater.edu>
2025-11-12 18:42:36 -0500

    Add files via upload

---
+++ fitting_functions.py
@@ -0,0 +1,10 @@
+def linear (m, x, b):
+    return m * x + b
+
+def slope_units (x_units, y_units):
+    x_units = x_units.rstrip("s")
+    y_units = y_units.rstrip("s")
+    return y_units + "/" + x_units
+
+def print_equation (m, b, y_units, x_units):
+    return print("The equation of the line is: y = ", str(m), slope_units, "x + " str(b), x_units)
